d67e3f3cc29dcace21729f7951ef9eb360b2cd4c
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-12-15 22:47:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Tell Next.js to use the 'src' folder for pages
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack(config) {
+    config.resolve.modules.push(__dirname + '/src');
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
